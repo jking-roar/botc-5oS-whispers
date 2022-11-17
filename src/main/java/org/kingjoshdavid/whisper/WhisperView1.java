cd8535b603e561127d527d25e1f1be6501cff5ab
Josh King <kingjoshdavid@gmail.com>
2022-11-16 20:53:25 -0500

    namePreference

--- src/main/java/org/kingjoshdavid/whisper/WhisperView1.java
+++ src/main/java/org/kingjoshdavid/whisper/WhisperView1.java
@@ -1,5 +1,8 @@
 package org.kingjoshdavid.whisper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WhisperView1 {
     public static void main(String[] args) {
         String townsquare = "aardvark\n" +
@@ -8,11 +11,11 @@ public class WhisperView1 {
                 "dogfish\n" +
                 "elephant\n" +
                 "ferret\n" +
-                "giraffe\n" +
+                "¯\\_(ツ)_/¯ giraffe\n" +
                 "hunter\n" +
                 "iguana\n";
         String dayOne = "bat - elephant: 32\n" +
-                "bat - giraffe: 22\n" +
+                "bat - ¯\\_(ツ)_/¯ giraffe: 22\n" +
                 "bat - iguana: 20\n" +
                 "bat - hunter: 18\n" +
                 "elephant - iguana: 18\n" +
@@ -24,17 +27,17 @@ public class WhisperView1 {
                 "elephant - hunter: 13\n" +
                 "aardvark - hunter: 12\n" +
                 "cheetah - elephant: 12\n" +
-                "giraffe - iguana: 12\n" +
+                "¯\\_(ツ)_/¯ giraffe - iguana: 12\n" +
                 "aardvark - elephant: 11\n" +
                 "bat - dogfish: 11\n" +
-                "giraffe - hunter: 10\n" +
+                "¯\\_(ツ)_/¯ giraffe - hunter: 10\n" +
                 "aardvark - iguana: 9\n" +
                 "dogfish - hunter: 8\n" +
                 "hunter - iguana: 7\n" +
                 "cheetah - hunter: 4\n" +
                 "cheetah - ferret: 3\n" +
                 "dogfish - ferret: 3\n" +
-                "ferret - giraffe: 3\n" +
+                "ferret - ¯\\_(ツ)_/¯ giraffe: 3\n" +
                 "aardvark - bat: 2\n" +
                 "bat - cheetah: 2\n" +
                 "cheetah - iguana: 1\n" +
@@ -44,10 +47,10 @@ public class WhisperView1 {
                 "hunter - storyteller: 1\n" +
                 "iguana - storyteller: 1\n";
         String dayOneTwo = "ferret - hunter: 22\n" +
-                "bat - giraffe: 14\n" +
+                "bat - ¯\\_(ツ)_/¯ giraffe: 14\n" +
                 "bat - ferret: 13\n" +
                 "bat - iguana: 10\n" +
-                "giraffe - elephant: 7\n" +
+                "¯\\_(ツ)_/¯ giraffe - elephant: 7\n" +
                 "ferret - iguana: 5\n" +
                 "iguana - dogfish: 4\n" +
                 "iguana - hunter: 4\n" +
@@ -60,7 +63,7 @@ public class WhisperView1 {
 
         String dayOneThree = "bat - aardvark: 2\n" +
                 "bat - hunter: 1\n" +
-                "hunter - giraffe: 1\n" +
+                "hunter - ¯\\_(ツ)_/¯ giraffe: 1\n" +
                 "hunter - elephant: 1\n";
 
         String dayTwo = "dogfish - hunter: 15\n" +
@@ -68,8 +71,8 @@ public class WhisperView1 {
                 "dogfish - ferret: 11\n" +
                 "ferret - iguana: 11\n" +
                 "dogfish - iguana: 10\n" +
-                "giraffe - hunter: 10\n" +
-                "bat - giraffe: 9\n" +
+                "¯\\_(ツ)_/¯ giraffe - hunter: 10\n" +
+                "bat - ¯\\_(ツ)_/¯ giraffe: 9\n" +
                 "elephant - iguana: 8\n" +
                 "bat - dogfish: 6\n" +
                 "bat - hunter: 6\n" +
@@ -79,9 +82,9 @@ public class WhisperView1 {
                 "cheetah - hunter: 4\n" +
                 "aardvark - ferret: 3\n" +
                 "cheetah - dogfish: 3\n" +
-                "cheetah - giraffe: 3\n" +
+                "cheetah - ¯\\_(ツ)_/¯ giraffe: 3\n" +
                 "cheetah - iguana: 3\n" +
-                "ferret - giraffe: 3\n" +
+                "ferret - ¯\\_(ツ)_/¯ giraffe: 3\n" +
                 "cheetah - ferret: 2\n" +
                 "aardvark - bat: 1\n" +
                 "bat - elephant: 1\n" +
@@ -91,14 +94,14 @@ public class WhisperView1 {
                 "iguana - storyteller: 1\n";
 
         String dayThree = "bat - hunter: 42\n" +
-                "bat - giraffe: 20\n" +
-                "giraffe - hunter: 15\n" +
+                "bat - ¯\\_(ツ)_/¯ giraffe: 20\n" +
+                "¯\\_(ツ)_/¯ giraffe - hunter: 15\n" +
                 "bat - elephant: 12\n" +
                 "hunter - iguana: 12\n" +
-                "cheetah - giraffe: 8\n" +
+                "cheetah - ¯\\_(ツ)_/¯ giraffe: 8\n" +
                 "bat - cheetah: 6\n" +
                 "bat - dogfish: 6\n" +
-                "elephant - giraffe: 6\n" +
+                "elephant - ¯\\_(ツ)_/¯ giraffe: 6\n" +
                 "dogfish - iguana: 5\n" +
                 "elephant - hunter: 4\n" +
                 "ferret - iguana: 4\n" +
@@ -112,11 +115,14 @@ public class WhisperView1 {
                 "iguana - storyteller: 1\n";
 
 
-        new WhisperView(townsquare, dayOne, "dayOne");
-        new WhisperView(townsquare, dayOneTwo, "dayOneTwo");
-        new WhisperView(townsquare, dayOneThree, "dayOneThree");
+        Map<String, String> namePreferences = new HashMap<>();
+        namePreferences.put("¯\\_(ツ)_/¯ giraffe", "giraffe");
 
-        new WhisperView(townsquare, dayTwo, "dayTwo");
-        new WhisperView(townsquare, dayThree, "dayThree");
+        new WhisperView(townsquare, dayOne, "dayOne", namePreferences);
+        new WhisperView(townsquare, dayOneTwo, "dayOneTwo", namePreferences);
+        new WhisperView(townsquare, dayOneThree, "dayOneThree", namePreferences);
+
+        new WhisperView(townsquare, dayTwo, "dayTwo", namePreferences);
+        new WhisperView(townsquare, dayThree, "dayThree", namePreferences);
     }
 }
